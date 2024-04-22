522d5274d33f300f383f543262a70165b461b52c
Mehmet Mesut Sincar <mesut.sincar@paycore.com>
2024-04-22 11:04:02 +0300

    react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
+import { Container, Row ,Col} from "reactstrap";
 
 function App() {
+
+  let titleCategoryList = "Category List";
+  let titleProductList = "Product List";
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Row>
+          <Navi></Navi>
+        </Row>
+
+        <Row>
+          <Col xs="3">
+            <CategoryList title={titleCategoryList}></CategoryList>
+          </Col>
+
+          <Col xs="9">
+           <ProductList title={titleProductList}></ProductList>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
